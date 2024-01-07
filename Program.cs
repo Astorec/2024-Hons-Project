6a7f6bdb15c4475dade697f183eb188cae2a2bc4
astorec <rossa1298@gmail.com>
2024-01-07 23:14:31 +0000

    Changed the Names from MudBlazorTesting > HonsProject to keep inline with the project naming

--- Program.cs
+++ Program.cs
@@ -1,6 +1,6 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-using MudBlazorTesting;
+using HonsProject;
 using MudBlazor.Services;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
